a1b5276ea2719b919aa36413c024cbb009cb73d2
Kiriti29 <44779395+Kiriti29@users.noreply.github.com>
2020-03-09 23:33:32 +0530

    Updated binary tree with full functionality

--- data_structures/Trees/Binary Trees/binary_search_tree.py
+++ data_structures/Trees/Binary Trees/binary_search_tree.py
@@ -1,25 +1,66 @@
-from node import Node
+"""
+    author = Muktevi Kiriti
+"""
 
 
-class BinarySearchTree(object):
+class Node:
 
-    def __init__(self, root):
-        self.root = Node(root)
+    def __init__(self):
+        self.right = None
+        self.left = None
+        self.root = None
+        self.parent = None
 
-    def add_node(self, node):
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = Node()
+
+    def get_root(self):
+        return self.root
+
+    def get_parent(self, node):
+        return node.parent
+
+    def get_left_child(self, node):
+        return node.left
+
+    def get_right_child(self, node):
+        return node.right
+
+    def insert_node(self, value):
         root = self.root
+        if root.root is None:
+            root.root = value
+            return
         while True:
-            if node < root.root:
-                if root.left is None:
-                    root.left = Node(node)
-                    break
-                else:
+            if value < root.root:
+                if root.left:
                     root = root.left
-            else:
-                if root.right is None:
-                    root.right = Node(node)
                 else:
+                    new_node = Node()
+                    new_node.root = value
+                    new_node.parent = root
+                    root.left = new_node
+                    break
+            else:
+                if root.right:
                     root = root.right
+                else:
+                    new_node = Node()
+                    new_node.root = value
+                    new_node.parent = root
+                    root.right = new_node
+                    break
+        return
 
-    def delete_node(Self):
+    def delete_node(self):
         pass
+
+
+if __name__ == "__main__":
+    bt = BinarySearchTree()
+    test_input = [7, 5, 1, 2, 6, 8, 4]
+    for i in test_input:
+        bt.insert_node(i)
